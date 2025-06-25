b284be30bb30480d823c02fbd051a1fd367fbdf4
Chia-Yu Chang <chia-yu.chang@nokia-bell-labs.com>
2025-06-25 18:44:15 +0200

    tcp: L4S ECT(1) identifier and NEEDS_ACCECN for CC modules
    Two CA module flags are added in this patch. First, a new CA module
    flag (TCP_CONG_NEEDS_ACCECN) defines that the CA expects to negotiate
    AccECN functionality using the ECE, CWR and AE flags in the TCP header.
    The detailed AccECN negotiaotn during the 3WHS can be found in the
    AccECN spec:
      https://tools.ietf.org/id/draft-ietf-tcpm-accurate-ecn-28.txt
    
    Second, when ECN is negociated for a TCP flow, it defaults to use
    ECT(0) in the IP header. L4S service, however, needs to se ECT(1).
    This patch enables CA to control whether ECT(0) or ECT(1) should
    be used on a per-segment basis. A new flag (TCP_CONG_WANTS_ECT_1)
    defines the behavior expected by the CA when not-yet initialized for
    the connection. As such, it implicitely assumes that the CA also has
    the TCP_CONG_NEEDS_ECN set.
    
    Co-developed-by: Olivier Tilmans <olivier.tilmans@nokia.com>
    Signed-off-by: Olivier Tilmans <olivier.tilmans@nokia.com>
    Signed-off-by: Ilpo Järvinen <ij@kernel.org>
    Signed-off-by: Chia-Yu Chang <chia-yu.chang@nokia-bell-labs.com>

--- include/net/tcp_ecn.h
+++ include/net/tcp_ecn.h
@@ -561,7 +561,7 @@ static inline void tcp_ecn_send_synack(struct sock *sk, struct sk_buff *skb)
 		TCP_SKB_CB(skb)->tcp_flags &= ~TCPHDR_ECE;
 	else if (tcp_ca_needs_ecn(sk) ||
 		 tcp_bpf_ca_needs_ecn(sk))
-		INET_ECN_xmit(sk);
+		__INET_ECN_xmit(sk, tcp_ca_wants_ect_1(sk));
 
 	if (tp->ecn_flags & TCP_ECN_MODE_ACCECN) {
 		TCP_SKB_CB(skb)->tcp_flags &= ~TCPHDR_ACE;
@@ -587,7 +587,7 @@ static inline void tcp_ecn_send_syn(struct sock *sk, struct sk_buff *skb)
 	 * |       3        | Request AccECN connection |
 	 * +================+===========================+
 	 */
-	use_accecn = tcp_ecn == 3;
+	use_accecn = tcp_ecn == 3 || tcp_ca_needs_accecn(sk);
 	use_ecn = tcp_ecn == 1 || tcp_ecn == 4 ||
 		  tcp_ca_needs_ecn(sk) || bpf_needs_ecn || use_accecn;
 
@@ -602,7 +602,7 @@ static inline void tcp_ecn_send_syn(struct sock *sk, struct sk_buff *skb)
 
 	if (use_ecn) {
 		if (tcp_ca_needs_ecn(sk) || bpf_needs_ecn)
-			INET_ECN_xmit(sk);
+			__INET_ECN_xmit(sk, tcp_ca_wants_ect_1(sk));
 
 		TCP_SKB_CB(skb)->tcp_flags |= TCPHDR_ECE | TCPHDR_CWR;
 		if (use_accecn) {
